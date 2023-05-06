d826ffa28a1bf8b48408a8e77eeef2be580409eb
jactymilena <saej3101@usherbrooke.ca>
2023-05-06 19:44:09 -0400

    WIP

--- code/scripts/create_csv_file.py
+++ code/scripts/create_csv_file.py
@@ -6,9 +6,15 @@ import csv
 dir_path = 'data'
 onlyfiles = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
 
-with open('data-files.csv', 'w', newline='') as csvfile:
+with open('data-files2.csv', 'w', newline='') as csvfile:
     wr = csv.writer(csvfile)
+#     for i, f in enumerate(onlyfiles):
+#         wr.writerow([f"../data/{f};output/out-{i}.png;480"])
+
     for i, f in enumerate(onlyfiles):
-        wr.writerow([f"../data/{f};output/out-{i}.png;480"])
+        wr.writerow([f"../data/{onlyfiles[i % 10]};output/out-{i}.png;480"])
+
+
+
 
 
