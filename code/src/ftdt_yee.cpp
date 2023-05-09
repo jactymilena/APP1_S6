c147591786f413232ea7237f2055bdd175584423
jactymilena <saej3101@usherbrooke.ca>
2023-05-08 21:53:14 -0400

    Fix index calcul error

--- code/src/ftdt_yee.cpp
+++ code/src/ftdt_yee.cpp
@@ -23,8 +23,9 @@ static const int L = 3;
 // static const int L = 2;
 
 // Tampon générique à utiliser pour créer le fichier
-// static const int BUFFER_SIZE = N * M * K * L * sizeof(double); // vrai
-static const int BUFFER_SIZE = N * M * K * L * sizeof(double); // vrai
+static const int BUFFER_SIZE = N * M * K * L * sizeof(double); 
+static const int DATA_SIZE = N * M * K;
+
 
 // static const int BUFFER_SIZE = 2 * 2 * 2 * 2 * sizeof(double); // test
 // static const int MATRIX_SIZE = 2;
@@ -46,7 +47,64 @@ struct field_component {
     double y;
     double z;
 
+    field_component() {}
     field_component(double x_, double y_, double z_) : x(x_), y(y_), z(z_) {}
+};
+
+int get_mtx_index(int i, int j, int k) 
+{
+    return (M * N) * i + M * j + k; 
+}
+
+int get_mtx_index_4d(int i, int j, int k, int l)
+{
+    return get_mtx_index(i, j, k)*L + l;
+}
+
+class Matrix {
+    private:
+    std::vector<field_component> data;
+
+    public:
+    Matrix(double* mtx) {
+        for(int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                for(int k = 0; k < K; k++) {
+                    data.push_back(field_component(
+                        mtx[get_mtx_index_4d(i, j, k, 0)], 
+                        mtx[get_mtx_index_4d(i, j, k, 1)], 
+                        mtx[get_mtx_index_4d(i, j, k, 2)]));
+                }
+            }
+        }
+    }
+
+    field_component& operator()(int i, int j, int k)  { 
+        return data[get_mtx_index(i, j, k)]; 
+    }    
+
+    double& operator()(int i, int j, int k, int l)  { 
+        int idx = get_mtx_index(i, j, k);
+        if(l == 0)      
+            return data[idx].x;
+        else if(l == 1)      
+            return data[idx].y;
+        else if(l == 2)      
+            return data[idx].z;
+    }
+
+    int size() { return data.size(); }
+
+    void replace_slice(std::vector<field_component> tab, slice_range ranges[3]) {
+        int cpt = 0;
+        for (int i = ranges[0].start_index; i < ranges[0].end_index; i++) {
+            for (int j = ranges[1].start_index; j < ranges[1].end_index; j++) {
+                for(int k = ranges[2].start_index; k < ranges[2].end_index; k++) {     
+                    data[get_mtx_index(i, j, k)] = tab[cpt++];
+                }
+            }
+        }
+    }
 };
 
 
@@ -64,19 +122,18 @@ void ack_signal()
     std::cout << "" << std::endl;
 }
 
-int get_mtx_index(int i, int j, int k) 
+
+
+// int get_mtx_index_4d(int i, int j, int k, int l)
+// {
+//     return (M*N*K) * i + (M*N) * j + M* k + l;
+// }
+
+std::vector<field_component> feed_components(double* mtx) 
 {
-    return (M * N) * i + M * j + k;
-}
+    std::vector<field_component> components(1000000);
 
-int get_mtx_index_4d(int i, int j, int k, int l)
-{
-    return (M*N*K) * i + (M*N) * j + M* k + l;
-}
-
-std::vector<field_component>  feed_components(double* mtx) {
-    std::vector<field_component> components;
-
+    std::cerr << " start feed_components\n";
     for(int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             for(int k = 0; k < K; k++) {
@@ -84,17 +141,25 @@ std::vector<field_component>  feed_components(double* mtx) {
             }
         }
     }
+    std::cerr << " end feed_components\n";
 
     return components;
 }
 
-std::vector<double> slice(std::vector<double>  mtx, slice_range ranges[3]) 
+
+
+std::vector<double> slice(Matrix mtx, slice_range ranges[3], int axe) 
 {
     std::vector<double> tmp;    
     for (int i = ranges[0].start_index; i < ranges[0].end_index; i++) {
         for (int j = ranges[1].start_index; j < ranges[1].end_index; j++) {
-            for(int k = ranges[2].start_index; k < ranges[2].end_index; k++) {                
-                tmp.push_back(mtx[get_mtx_index(i, j, k)]);
+            for(int k = ranges[2].start_index; k < ranges[2].end_index; k++) {          
+                if(axe == 0)      
+                    tmp.push_back(mtx(i, j, k).x);
+                else if(axe == 1)      
+                    tmp.push_back(mtx(i, j, k).y);
+                else if(axe == 2)      
+                    tmp.push_back(mtx(i, j, k).z);
             }
         }
     }
@@ -120,22 +185,24 @@ void replace_mtx(double* mtx, std::vector<double> v)
     }
 }
 
-void curl(double* mtx)
+void curl(Matrix mtx_E)
 {
     // TODO: make a copy of mtx into E, put zeros into mtx
 
 
     // make a copy of actual E matrix
-    std::vector<double> mtx_E (mtx, mtx + N * M * K);
+    // std::vector<double> mtx_E (mtx, mtx + N * M * K);
 
     std::cerr << "E\n";
     // std::cerr << mtx_E.size();    
 
-    slice_range ranges[3] = {{0, N}, {1, M}, {0, K}};
-    std::vector<double> m1 = slice(mtx_E, ranges);
+    // slice_range ranges[3] = {{0, N}, {1, M}, {0, K}};
+    // std::vector<field_component> m1 = slice(mtx_E, ranges);
 
-    slice_range ranges2[3] = {{0, N}, {0, M-1}, {0, K}};
-    std::vector<double> m2 = slice(mtx_E, ranges2);
+    // slice_range ranges2[3] = {{0, N}, {0, M-1}, {0, K}};
+    // std::vector<field_component> m2 = slice(mtx_E, ranges2);
+
+    // replace_slice
 
 
     // std::cout << "tmp "; 
@@ -146,8 +213,8 @@ void curl(double* mtx)
     // for (double i: tmp)
     //     std::cout << i << ' ';
 
-    diff_vectors(m1, m2);
-    replace_mtx(mtx, m1);
+    // diff_vectors(m1, m2);
+    // replace_mtx(mtx, m1);
     
     /* TODO: 
         - put zeros into mtx before addition
@@ -167,7 +234,7 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
-    wait_signal();
+    // wait_signal();  // DECOMMENTER
 
     // Création d'un fichier "vide" (le fichier doit exister et être d'une
     // taille suffisante avant d'utiliser mmap).
@@ -180,7 +247,7 @@ int main(int argc, char const *argv[])
 
     // On signale que le fichier est prêt.
     std::cerr << "CPP:  File ready." << std::endl;
-    ack_signal(); 
+    // ack_signal(); // DECOMMENTER
 
     // On ré-ouvre le fichier et le passe à mmap(...). Le fichier peut ensuite
     // être fermé sans problèmes (mmap y a toujours accès, jusqu'à munmap.)
@@ -197,10 +264,14 @@ int main(int argc, char const *argv[])
     // Pointeur format double qui représente la matrice partagée:
     double* mtx = (double*)shm_mmap; // rergarder pour utiliser des vectors a la place 
 
-    wait_signal();
+    // wait_signal();  // DECOMMENTER
     std::cerr << "Before curl E\n";
 
-    std::vector<field_component> mtx_E = feed_components(mtx);
+    Matrix mtx_E(mtx);
+
+    // std::vector<field_component> mtx_E = feed_components(mtx);
+
+    std::cerr << mtx_E.size() << std::endl;
     
     // ack_signal(); //DECOMMENTER
     // curl(mtx);
